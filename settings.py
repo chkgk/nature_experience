2fc70335697da6e20a5c21edf6ae6f9d30e5e0de
Christian König <koenig.kersting@gmail.com>
2018-08-07 13:35:21 +0200

    some adjustments to structure and templates

--- settings.py
+++ settings.py
@@ -13,63 +13,87 @@ SESSION_CONFIG_DEFAULTS = {
 
 SESSION_CONFIGS = [
     {
-       'name': 'zero_intro_human',
-       'display_name': "Intro - Human",
-       'num_demo_participants': 1,
-       'app_sequence': ['zero_intro'],
-       'treatment': 'human'
+        'name': 'zero_intro_human',
+        'display_name': "Intro - Human",
+        'num_demo_participants': 1,
+        'app_sequence': ['zero_intro'],
+        'treatment': 'human'
     },
     {
-       'name': 'zero_main_human',
-       'display_name': "Main Part - Human",
-       'num_demo_participants': 4,
-       'app_sequence': ['zero_main_human'],
-       'treatment': 'human'
+        'name': 'zero_main_1_human',
+        'display_name': "Decision Part 1 - Human",
+        'num_demo_participants': 4,
+        'app_sequence': ['zero_main_human'],
+        'treatment': 'human',
+        'decision1': True,
     },
     {
-       'name': 'zero_outro_human',
-       'display_name': "Outro - Human",
-       'num_demo_participants': 1,
-       'app_sequence': ['zero_outro'],
-       'treatment': 'human'
+        'name': 'zero_main_2_human',
+        'display_name': "Decision Part 2 - Human",
+        'num_demo_participants': 4,
+        'app_sequence': ['zero_main_human'],
+        'treatment': 'human',
+        'decision1': False,
     },
     {
-       'name': 'zero_human',
-       'display_name': "Full Experiment - Human",
-       'num_demo_participants': 1,
-       'app_sequence': ['zero_intro', 'zero_main_human', 'zero_outro'],
-       'treatment': 'human'
+        'name': 'zero_outro_human',
+        'display_name': "Outro - Human",
+        'num_demo_participants': 1,
+        'app_sequence': ['zero_outro'],
+        'treatment': 'human'
     },
     {
-       'name': 'zero_intro_bot',
-       'display_name': "Intro - Computer",
-       'num_demo_participants': 1,
-       'app_sequence': ['zero_intro'],
-       'treatment': 'computer'
+        'name': 'zero_human',
+        'display_name': "Full Experiment - Human",
+        'num_demo_participants': 1,
+        'app_sequence': ['zero_intro', 'zero_main_1_human', 'zero_main_1_human', 'zero_outro'],
+        'treatment': 'human'
     },
     {
-       'name': 'zero_main_bot',
-       'display_name': "Main Part - Computer",
-       'num_demo_participants': 1,
-       'app_sequence': ['zero_main_bot'],
-       'treatment': 'computer'
+        'name': 'zero_intro_bot',
+        'display_name': "Intro - Computer",
+        'num_demo_participants': 1,
+        'app_sequence': ['zero_intro'],
+        'treatment': 'computer'
     },
     {
-       'name': 'zero_outro_bot',
-       'display_name': "Outro - Computer",
-       'num_demo_participants': 1,
-       'app_sequence': ['zero_outro'],
-       'treatment': 'computer'
+        'name': 'zero_main_bot',
+        'display_name': "Main Part - Computer",
+        'num_demo_participants': 1,
+        'app_sequence': ['zero_main_bot'],
+        'treatment': 'computer'
     },
     {
-       'name': 'zero_bot',
-       'display_name': "Full Experiment - Computer",
-       'num_demo_participants': 1,
-       'app_sequence': ['zero_intro', 'zero_main_bot', 'zero_outro'],
-       'treatment': 'computer'
+        'name': 'zero_main_1_bot',
+        'display_name': "Decision Part 1 - Computer",
+        'num_demo_participants': 1,
+        'app_sequence': ['zero_main_bot'],
+        'treatment': 'computer',
+        'decision1': True,
+    },
+    {
+        'name': 'zero_main_1_bot',
+        'display_name': "Decision Part 2 - Computer",
+        'num_demo_participants': 1,
+        'app_sequence': ['zero_main_bot'],
+        'treatment': 'computer',
+        'decision1': False
+    },
+    {
+        'name': 'zero_outro_bot',
+        'display_name': "Outro - Computer",
+        'num_demo_participants': 1,
+        'app_sequence': ['zero_outro'],
+        'treatment': 'computer'
+    },
+    {
+        'name': 'zero_bot',
+        'display_name': "Full Experiment - Computer",
+        'num_demo_participants': 1,
+        'app_sequence': ['zero_intro', 'zero_main_1_bot', 'zero_main_2_bot', 'zero_outro'],
+        'treatment': 'computer'
     },
 ]
-
 
 # ISO-639 code
 # for example: de, fr, ja, ko, zh-hans
@@ -80,7 +104,6 @@ REAL_WORLD_CURRENCY_CODE = 'USD'
 USE_POINTS = False
 
 ROOMS = []
-
 
 # AUTH_LEVEL:
 # this setting controls which parts of your site are freely accessible,
@@ -99,7 +122,6 @@ ADMIN_USERNAME = 'admin'
 # for security, best to set admin password in an environment variable
 ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
 
-
 # Consider '', None, and '0' to be empty/false
 DEBUG = (environ.get('OTREE_PRODUCTION') in {None, '', '0'})
 
@@ -110,3 +132,4 @@ SECRET_KEY = '(js*7-e9%&_d*0j3%0jd&m$_)qvppc0tp_%7pzhqq-mt8=k6k@'
 
 # if an app is included in SESSION_CONFIGS, you don't need to list it here
 INSTALLED_APPS = ['otree']
+EXTENSION_APPS = ['otree_mturk_utils']
