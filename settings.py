31835c09bc7551a52bf4dacd4e41a94c61fc0635
Christian König <koenig.kersting@gmail.com>
2018-09-27 09:05:36 +0200

    mturk again

--- settings.py
+++ settings.py
@@ -1,5 +1,52 @@
 from os import environ
 
+AWS_ACCESS_KEY_ID = environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = environ.get('AWS_SECRET_ACCESS_KEY')
+
+mturk_hit_settings = {
+    'keywords': ['bonus', 'study', 'decision making'],
+    'title': 'Research in decision making',
+    'description': 'Participate in a game and a short survey. Please note that the task is to be completed within 10-15 minutes as you are matched with a co-player.',
+    'frame_height': 500,
+    'preview_template': 'zero_intro/MTurkPreview.html',
+    'minutes_allotted_per_assignment': 25,
+    'expiration_hours': 3 * 24,  # 3 days
+    # PRODUCTION ONLY
+    # this would grant florians qualification, if we run it through his account.
+    # 'grant_qualification_id': '3DA2M59FDW8FXK3OTJT76I16UG0RXG',# to prevent retakes
+
+    # alternatively, if we use Christian's account:
+    #'grant_qualification_id': '3X4G950TG9JM27SOMLGT6VJ1IU8P1C',
+    # SANDBOX ONLY
+
+    # 'grant_qualification_id': '3TW6VZEPPW5UL7N9GRPX9CW4P7VXPS',# to prevent retakes
+    'qualification_requirements': [
+        # # PRODUCTION ONLY
+        # {
+        #     'QualificationTypeId': "00000000000000000071",
+        #     'Comparator': "EqualTo",
+        #     'LocaleValues': [{'Country': "US"}]
+        # },
+        # # qualification granted by Florian's Experiments
+        # {
+        #     'QualificationTypeId': "3DA2M59FDW8FXK3OTJT76I16UG0RXG",
+        #     'Comparator': "DoesNotExist",
+        # },
+        # # qualification granted by Christian's runs
+        # {
+        #     'QualificationTypeId': "3X4G950TG9JM27SOMLGT6VJ1IU8P1C",
+        #     'Comparator': "DoesNotExist",
+        # }
+        # SANDBOX ONLY
+        # {
+        #    'QualificationTypeId': "3TW6VZEPPW5UL7N9GRPX9CW4P7VXPS",
+        #    'Comparator': "DoesNotExist",
+        # }
+    ],
+}
+
+
+
 # if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
 # in SESSION_CONFIGS, except those that explicitly override it.
 # the session config can be accessed from methods in your apps as self.session.config,
@@ -79,48 +126,6 @@ SESSION_CONFIGS = [
     # },
 ]
 
-mturk_hit_settings = {
-    'keywords': ['bonus', 'study', 'decision making'],
-    'title': 'Research in decision making',
-    'description': 'Participate in a game and a short survey. Please note that the task is to be completed within 10-15 minutes as you are matched with a co-player.',
-    'frame_height': 500,
-    'preview_template': 'zero_intro/MTurkPreview.html',
-    'minutes_allotted_per_assignment': 25,
-    'expiration_hours': 3 * 24,  # 3 days
-    # PRODUCTION ONLY
-    # this would grant florians qualification, if we run it through his account.
-    # 'grant_qualification_id': '3DA2M59FDW8FXK3OTJT76I16UG0RXG',# to prevent retakes
-
-    # alternatively, if we use Christian's account:
-    #'grant_qualification_id': '3X4G950TG9JM27SOMLGT6VJ1IU8P1C',
-    # SANDBOX ONLY
-
-    # 'grant_qualification_id': '3TW6VZEPPW5UL7N9GRPX9CW4P7VXPS',# to prevent retakes
-    'qualification_requirements': [
-        # # PRODUCTION ONLY
-        # {
-        #     'QualificationTypeId': "00000000000000000071",
-        #     'Comparator': "EqualTo",
-        #     'LocaleValues': [{'Country': "US"}]
-        # },
-        # # qualification granted by Florian's Experiments
-        # {
-        #     'QualificationTypeId': "3DA2M59FDW8FXK3OTJT76I16UG0RXG",
-        #     'Comparator': "DoesNotExist",
-        # },
-        # # qualification granted by Christian's runs
-        # {
-        #     'QualificationTypeId': "3X4G950TG9JM27SOMLGT6VJ1IU8P1C",
-        #     'Comparator': "DoesNotExist",
-        # }
-        # SANDBOX ONLY
-        # {
-        #    'QualificationTypeId': "3TW6VZEPPW5UL7N9GRPX9CW4P7VXPS",
-        #    'Comparator': "DoesNotExist",
-        # }
-    ],
-}
-
 # ISO-639 code
 # for example: de, fr, ja, ko, zh-hans
 LANGUAGE_CODE = 'en'
@@ -159,6 +164,3 @@ SECRET_KEY = '(js*7-e9%&_d*0j3%0jd&m$_)qvppc0tp_%7pzhqq-mt8=k6k@'
 # if an app is included in SESSION_CONFIGS, you don't need to list it here
 INSTALLED_APPS = ['otree']
 EXTENSION_APPS = ['otree_mturk_utils']
-
-AWS_ACCESS_KEY_ID = environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = environ.get('AWS_SECRET_ACCESS_KEY')
