bafe584e6e9e417878606ee405fcd8cd9ad0d528
Christian König <koenig.kersting@gmail.com>
2019-03-13 16:53:33 +0100

    added tests

--- v3_results_2/tests.py
+++ v3_results_2/tests.py
@@ -7,4 +7,15 @@ from .models import Constants
 class PlayerBot(Bot):
 
     def play_round(self):
+        if self.group.ball_green:
+            if self.player.action and self.player.others_action:
+                assert self.player.room_payoff == c(0)
+            elif self.player.action and not self.player.others_action:
+                assert self.player.room_payoff == c(3)
+            elif not self.player.action and self.player.others_action:
+                assert self.player.room_payoff == c(1)
+            else:
+                assert self.player.room_payoff == c(1)
+        else:
+            assert self.player.room_payoff == c(0)
         yield (pages.Results)
